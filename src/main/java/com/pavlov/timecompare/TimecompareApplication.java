62e3d94cc96637c93dbd232c072298f445eaa0c1
root <localducks@gmail.com>
2017-05-31 22:37:32 +0300

    initial commit

---
+++ src/main/java/com/pavlov/timecompare/TimecompareApplication.java
@@ -0,0 +1,12 @@
+package com.pavlov.timecompare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimecompareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimecompareApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pavlov/timecompare/TimecompareApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pavlov.timecompare;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TimecompareApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
